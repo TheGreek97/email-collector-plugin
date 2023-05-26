895133d5677ab22a309a6ef3c0dd6662797c2d1e
dnlKnots <d.palmitesta@studenti.uniba.it>
2023-05-26 11:45:00 +0200

    Inserito il codice. Al momento è implementato solo la caratteristica plain text

--- PhisingDataCollector/MailData.cs
+++ PhisingDataCollector/MailData.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
 
+using System.Text.RegularExpressions;
+
 namespace PhisingDataCollector
 {
+
     class MailData
     {
+        
+        private Outlook.MailItem mail;
+        private Outlook.MailItem mailOriginal;
 
+        public bool plain_Text;
+        public int number_of_html_comments_tag;
+
+        public MailData(Outlook.MailItem pMail)
+        {
+            mail = pMail;
+            mailOriginal = pMail;
+
+            //Bring all the mail to uppercase to semplify the search
+            mail.Body = mail.Body.ToUpper();
+            mail.HTMLBody = mail.HTMLBody.ToUpper();
+
+            Valorize_plain_Text();
+            Valorize_Number_of_html_comments_tag();
+        }
+
+        private void Valorize_plain_Text()
+        {
+            plain_Text = mail.Body == mail.HTMLBody;
+        }
+
+        private void Valorize_Number_of_html_comments_tag()
+        {
+            Regex rx = new Regex(@"<!--\b");
+
+            number_of_html_comments_tag = rx.Matches(mail.HTMLBody).Count;
+        }
+
+       
     }
 }
--- PhisingDataCollector/ThisAddIn.cs
+++ PhisingDataCollector/ThisAddIn.cs
@@ -7,14 +7,40 @@ using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
 
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
 
 namespace PhisingDataCollector
 {
     public partial class ThisAddIn
     {
+        List<MailData> dataList = new List<MailData>();
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            Outlook.MAPIFolder inbox = Globals.ThisAddIn.Application.Session.DefaultStore.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
 
+            foreach (Outlook.MailItem item in inbox.Items)
+            {
+                if (item != null)
+                {
+                    getMailData(item);
+                }
+            }
+
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true,
+            };
+            StreamWriter writer = new StreamWriter(@"C:\Users\dnlpl\Desktop\RecivedEmail1.txt");
+            string json = JsonSerializer.Serialize(dataList, options);
+            writer.WriteLine(json);
+            writer.Close();
+        }
+
+        private void getMailData(Outlook.MailItem pMail)
+        {
+            MailData md = new MailData(pMail);
+            dataList.Add(md);
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
