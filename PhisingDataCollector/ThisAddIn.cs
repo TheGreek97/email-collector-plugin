24467b7913e3c215f0fd6139c533b28fc015a41f
dnlpl <dnlpl@PALMI>
2023-05-26 11:20:06 +0200

    Aggiungere i file di progetto.

---
+++ PhisingDataCollector/ThisAddIn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+using System.Text.Json;
+
+namespace PhisingDataCollector
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Nota: Outlook non genera più questo evento. Se è presente codice che 
+            //    deve essere eseguito all'arresto di Outlook, vedere https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region Codice generato da VSTO
+
+        /// <summary>
+        /// Metodo richiesto per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
